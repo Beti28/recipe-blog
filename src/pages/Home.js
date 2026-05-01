5d1331859dcd1462574f718c4aca27c1ba77cea8
Beti28 <beatrisivanova05@gmail.com>
2026-05-01 20:24:41 +0300

    Fetch and display recipes on Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,35 @@
+import { useState, useEffect } from 'react';
+
 function Home() {
+  const [recipes, setRecipes] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:8000/recipes')
+      .then(response => response.json())
+      .then(data => setRecipes(data)) 
+      .catch(error => console.error('Грешка при зареждане:', error));
+  }, []);
+
   return (
-    <div>
-      <h2>Начална страница</h2>
-      <p>Тук ще се показват рецептите.</p>
+    <div style={{ padding: '20px' }}>
+      <h2>Нашите Рецепти</h2>
+      
+      <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', justifyContent: 'center' }}>
+
+        {recipes.map(recipe => (
+          <div key={recipe.id} style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '8px', width: '300px' }}>
+            <h3>{recipe.title}</h3>
+            <img 
+              src={recipe.image} 
+              alt={recipe.title} 
+              style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '8px' }} 
+            />
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
